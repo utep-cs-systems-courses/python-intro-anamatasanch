e77a9671daff260899548b4bb7586a14adee7957
anamatasanch <almatasanch@miners.utep.edu>
2020-09-01 17:13:15 -0600

    Simple ocurrence counter

--- wordcounter.py
+++ wordcounter.py
@@ -3,13 +3,21 @@ import re
 def main():
     print("Welcome to Ana's word counter.")
     dictFile = input("Please enter the name of your dictionary file. i.e. declaration.txt\n")
+    keyFile = input("Please enter the name of your key file.\n")
+    ocurrences = {}
+    
     try:
         reader = open(dictFile, "r")
         text = reader.read()
         text = text.lower()
-        words = re.split('; |, |\n|\.| |\. |,|:',text)
-        print(words)
-        print(len(words))
+        words = re.split('; |, |\n|\.| |\. |,|:|--',text)
+        for word in words:
+            if len(word)>0:
+                if word in ocurrences:
+                    ocurrences[word] += 1
+                else:
+                    ocurrences[word] = 1
+        print(sorted(ocurrences.items(), key=lambda item: item[0]))
         reader.close()
     except:
         print("Coouldn't find your file.. Make sure to put it in the same directory.")
