e957724a6b2bddce384fabd6c3319a9bb39ade90
anamatasanch <almatasanch@miners.utep.edu>
2020-09-01 17:03:40 -0600

    Simple split

--- wordcounter.py
+++ wordcounter.py
@@ -1,9 +1,15 @@
+import re
+
 def main():
     print("Welcome to Ana's word counter.")
     dictFile = input("Please enter the name of your dictionary file. i.e. declaration.txt\n")
     try:
         reader = open(dictFile, "r")
-        print(reader.read())
+        text = reader.read()
+        text = text.lower()
+        words = re.split('; |, |\n|\.| |\. |,|:',text)
+        print(words)
+        print(len(words))
         reader.close()
     except:
         print("Coouldn't find your file.. Make sure to put it in the same directory.")
