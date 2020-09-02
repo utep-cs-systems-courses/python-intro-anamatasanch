bde41856c2d16f1bed2026bf72f1981ac1079111
anamatasanch <almatasanch@miners.utep.edu>
2020-09-02 00:26:27 -0600

    Simple write

--- wordcounter.py
+++ wordcounter.py
@@ -9,19 +9,31 @@ def main():
     try:
         reader = open(dictFile, "r")
         text = reader.read()
+        reader.close()
         text = text.lower()
-        words = re.split('; |, |\n|\.| |\. |,|:|--',text)
+        words = re.split('; |, |\n|\.| |\. |,|:|--|-',text)
+        
         for word in words:
             if len(word)>0:
+                word = word.replace("'", "")
                 if word in ocurrences:
                     ocurrences[word] += 1
                 else:
                     ocurrences[word] = 1
-        print(sorted(ocurrences.items(), key=lambda item: item[0]))
-        reader.close()
-    except:
+        sorted_ocurrences = sorted(ocurrences.items(), key=lambda item: item[0])
+    except IOError:
         print("Coouldn't find your file.. Make sure to put it in the same directory.")
     
+    try:
+        f = open(keyFile, "w")
+        for ocurrence in sorted_ocurrences:
+            f.write(ocurrence[0]+" "+str(ocurrence[1])+"\n")
+        f.close()
+    except IOError:
+        f = open(keyFile, "x")
+        for ocurrence in sorted_ocurrences:
+            f.write(ocurrence[0]+" "+str(ocurrence[1])+"\n")
+        f.close()
     
     
 if __name__ == "__main__":
